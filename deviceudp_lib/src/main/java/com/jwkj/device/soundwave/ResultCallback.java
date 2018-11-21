70c3c12fb944f8cd93e5b1022a62c41382113366
1105010206 <1925879075@qq.com>
2018-11-21 10:22:50 +0800

    使用jcenter库引用基础UDP库

--- deviceudp_lib/src/main/java/com/jwkj/device/soundwave/ResultCallback.java
+++
@@ -1,9 +0,0 @@
-package com.jwkj.device.soundwave;
-
-import com.jwkj.device.shake.ShakeListener;
-
-
-public abstract class ResultCallback extends ShakeListener {
-
-    public abstract void onStopSend();
-}
